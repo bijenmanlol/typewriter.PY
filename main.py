994fac8974ba8abb4f70fb5008677374be73e42e
drooler <85669046+bijenmanlol@users.noreply.github.com>
2022-12-05 21:27:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import typewriter, shoppingList
+import typewriter
 
 typewriter.define(True)
 
-for i in range(1000):
-    typewriter.type(f"hondenmelk")
+typewriter.type(f"{typewriter.bold}Typewriter.PY{typewriter.reset} module by {typewriter.color_lightblue} @drooler (https://drooler.tk/){typewriter.reset}")
+typewriter.type(f"{typewriter.dim}Read the readme.md (https://github.com/bijenmanlol/typewriter.PY/readme.md/) for more info{typewriter.reset}")
